f4d34a85573d95692e91e529730b7e75d1bb1804
felg <felipegalind0@outlook.com>
2026-05-17 16:21:56 -0500

    Updates

--- assets/selection.vertex-BgO2f5Ec.js
+++ assets/selection.vertex-BgO2f5Ec.js
@@ -1,4 +1,4 @@
-import{t as e}from"./index-CsTBIWz2.js";var t=`selectionVertexShader`,n=`attribute vec3 position;
+import{t as e}from"./index-B023dEkj.js";var t=`selectionVertexShader`,n=`attribute vec3 position;
 #ifdef INSTANCES
 attribute float instanceSelectionId;
 #endif
