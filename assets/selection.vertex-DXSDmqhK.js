fd18475e86a126046c7978462fab5eccc4c0c3fb
felg <felipegalind0@outlook.com>
2026-05-17 18:59:02 -0500

    Updates

--- assets/selection.vertex-DXSDmqhK.js
+++ assets/selection.vertex-DXSDmqhK.js
@@ -1,4 +1,4 @@
-import{t as e}from"./index-CUh6hCo1.js";var t=`selectionVertexShader`,n=`attribute position: vec3f;
+import{t as e}from"./index-CqwFBtzT.js";var t=`selectionVertexShader`,n=`attribute position: vec3f;
 #ifdef INSTANCES
 attribute instanceSelectionId: f32;
 #endif
--- assets/selection.vertex-hBrlZjOf.js
+++ assets/selection.vertex-hBrlZjOf.js
@@ -1,4 +1,4 @@
-import{t as e}from"./index-CUh6hCo1.js";var t=`selectionVertexShader`,n=`attribute vec3 position;
+import{t as e}from"./index-CqwFBtzT.js";var t=`selectionVertexShader`,n=`attribute vec3 position;
 #ifdef INSTANCES
 attribute float instanceSelectionId;
 #endif
